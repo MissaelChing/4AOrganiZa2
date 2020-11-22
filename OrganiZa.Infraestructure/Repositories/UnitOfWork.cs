19bb618a66a90ddafa6f8e6cd20051685fe872f9
MissaelChing <Missa_Ching@hotmail.com>
2020-11-22 00:09:03 -0600

    api

---
+++ OrganiZa.Infraestructure/Repositories/UnitOfWork.cs
@@ -0,0 +1,42 @@
+﻿using OrganiZa.Domain.Interfaces;
+using OrganiZa.Infraestructure.Data;
+using OrganiZa.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrganiZa.Infraestructure.Repositories
+{
+    public sealed class UnitOfWork : IUnitOfWork
+    {
+        private readonly OrganizarecContext _context;
+
+     
+        private readonly IRepository<User> _usuarioRepository;
+
+        public UnitOfWork(OrganizarecContext context)
+        {
+            this._context = context;
+        }
+
+      
+        public IRepository<User> UsuarioRepository => _usuarioRepository ?? new SQLRepository<User>(_context);
+
+        public void Dispose()
+        {
+            if (_context == null)
+                _context.Dispose();
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task SaveChangesAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
