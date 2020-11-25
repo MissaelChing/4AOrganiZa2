3ad3317b9a12c3a43ddf20128cc01ad51f442acb
MissaelChing <Missa_Ching@hotmail.com>
2020-11-24 20:23:37 -0600

    cambios

---
+++ OrganiZa.Domain/DTOs/AdministradorResponseDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrganiZa.Domain.DTOs
+{
+    public class AdministradorResponseDto
+    {
+        public string Rolusuario { get; set; }
+        public string NombreAd { get; set; }
+        public int? UsersId { get; set; }
+        public int? EscuelaModelsId { get; set; }
+        public int? CalendarioModelsId { get; set; }
+
+        public bool Status { get; set; }
+        public DateTime CreateAt { get; set; }
+        public int? CreatedBy { get; set; }
+        public DateTime? UpdateAt { get; set; }
+        public int? UpdatedBy { get; set; }
+    }
+}
