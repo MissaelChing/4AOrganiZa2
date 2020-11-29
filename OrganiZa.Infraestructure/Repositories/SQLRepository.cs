dbe8ebd133c6be800b4ead34007666d544194025
Ravell <joelaaron19@outlook.es>
2020-11-29 15:59:18 -0600

    cambios

--- OrganiZa.Infraestructure/Repositories/SQLRepository.cs
+++ OrganiZa.Infraestructure/Repositories/SQLRepository.cs
@@ -13,6 +13,7 @@ namespace OrganiZa.Infraestructure.Repositories
 {
     public class SQLRepository<T> : IRepository<T> where T : BaseEntity
     {
+
         protected readonly OrganizarecContext _context;
         private DbSet<T> _entities;
 
