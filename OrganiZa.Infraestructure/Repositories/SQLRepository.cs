d13007da4946b36dc55068b0b6cf45a5751c0042
Ravell <joelaaron19@outlook.es>
2020-11-29 15:57:14 -0600

    cambios

--- OrganiZa.Infraestructure/Repositories/SQLRepository.cs
+++ OrganiZa.Infraestructure/Repositories/SQLRepository.cs
@@ -9,7 +9,6 @@ using System.Linq.Expressions;
 using System.Text;
 using System.Threading.Tasks;
 
-
 namespace OrganiZa.Infraestructure.Repositories
 {
     public class SQLRepository<T> : IRepository<T> where T : BaseEntity
