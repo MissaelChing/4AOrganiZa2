bb52125cfb3ab87e13c6604c71bb75d107d14d49
MissaelChing <Missa_Ching@hotmail.com>
2020-12-04 15:39:59 -0600

    Agregando el primer test

---
+++ OrganiZa.Web/Validacion/LoginValidator.cs
@@ -0,0 +1,21 @@
+﻿using FluentValidation;
+using FluentValidation.AspNetCore;
+using OrganiZa.Web.Models;
+
+namespace OrganiZa.Web.Validacion
+{
+    public class LoginValidator : AbstractValidator<LoginModel>
+    {
+        public LoginValidator()
+        {
+            RuleFor(login => login.Usuario)
+                .NotNull().WithMessage("Este campo no puede ser null")
+                .NotEmpty().WithMessage("Este campo no puede ser vacio");
+            RuleFor(login => login.Contraseña)
+                .NotNull().WithMessage("Este campo no puede ser null")
+                .NotEmpty().WithMessage("Este campo no puede ser vacio")
+                .Length(2, 15);
+            RuleFor(login => login.status).NotEqual(true).WithMessage("Correo y contraseña no validos");
+        }
+    }
+}
